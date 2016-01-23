9a2021ab8106a608957b3b3f3c06f206bdab8fc3
Sagar kumar <sagar.kumar@kiwitech.com>
2016-01-23 15:27:57 +0530

    adsfa

--- www/hello.js
+++ www/hello.js
@@ -7,25 +7,25 @@ module.exports = {
 	if(direction)
 		args.direction = direction;
 	cordova.exec(null, null, "Hello", "movePageHandler", [args]);
-}
+},
 
 callWebBrowserHomePage : function() {
     
     var args = {};
 	cordova.exec(null, null, "Hello", "callWebBrowserHomePage", [args]);
-}
+},
 
 callArticlePage : function() {
     
     var args : {};
 	cordova.exec(null, null, "Hello", "callArticlePage", [args]);
-}
+},
 
 callWebBrowserPage : function() {
     
     var args = {};
 	cordova.exec(null, null, "Hello", "callWebBrowserPage", [args]);
-}
+},
 
 callOfflinePDF : function(filePath) {
     
@@ -33,24 +33,24 @@ callOfflinePDF : function(filePath) {
 	if(filePath)
 		args.filePath = filePath;
 	cordova.exec(null, null, "Hello", "callOfflinePDF", [args]);
-}
+},
 
 startLoader: function() {
     
     var args = {};
 	cordova.exec(null, null, "Hello", "startLoader", [args]);
-}
+},
 
 stopLoader : function() {
     
     var args = {};
 	cordova.exec(null, null, "Hello", "stopLoader", [args]);
-}
+},
 
 callback : function() {
     
     var args = {};
 	cordova.exec(null, null, "Hello", "callback", [args]);
-}
-}
+},
+};
 
